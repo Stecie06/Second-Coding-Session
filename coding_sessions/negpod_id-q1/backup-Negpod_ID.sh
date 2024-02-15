da97cfe242ff9cab115daddcd2d332349837ddcc
Wilsons-Navid <wadotiwawil@gmail.com>
2024-02-15 04:28:38 -0800

    Backup

--- coding_sessions/negpod_id-q1/backup-Negpod_ID.sh
+++ coding_sessions/negpod_id-q1/backup-Negpod_ID.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-# Get negpod ID from first argument
 negpod_id=$1
 
-# Check if negpod ID is provided
 if [[ -z "$negpod_id" ]]; then
-  echo "Error
+  echo "Error: Please provide your Negpod ID as an argument."
+  exit 1
+fi
+
+remote_host="2f05c1f8800b.3be8ebfc.alu-cod.online"
+remote_username="2f05c1f8800b"
+remote_password="d4a1d225d0abda9549d8"
+remote_directory="/summative/1023-2024j"
+
+backup_filename="backup-Negpod_${negpod_id}.tar.gz"
+
+tar -zcf "$backup_filename" .
+
+scp -o StrictHostKeyChecking=no "$backup_filename" "$remote_username@$remote_host:$remote_directory"
+
+rm "$backup_filename"
+
+echo "Backup completed successfully."
