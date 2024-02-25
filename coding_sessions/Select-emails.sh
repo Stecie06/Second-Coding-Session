b389b8eb77049451accbc5db088ea57233bab471
Angelo Verlain <geoangercola@gmail.com>
2024-02-25 14:48:49 +0200

    make the select-emails function easier

--- coding_sessions/Select-emails.sh
+++ coding_sessions/Select-emails.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -Eo '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b' students-list_1023.txt > student-emails.txt
+cat students-list_1023.txt | cut -d, -f2 > student-emails.txt
